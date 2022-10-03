477620ba89ec1269af4d01afbc9e49f9b12a23a6
Arif Mehedi <100073639+Arifmehdi@users.noreply.github.com>
2022-10-03 22:46:58 +0600

    This is my first commit

---
+++ class.py
@@ -0,0 +1,10 @@
+print('hello')
+
+phone = 1303182771
+
+# 0123456789;
+
+print(phone)
+
+if(phone == 1303182771):
+    print('ok')
---
+++ class2.py
@@ -0,0 +1,26 @@
+# x = 5
+
+
+# print(type(x))
+
+# y = str(5)
+
+# print(type(y))
+
+# z= bool(y)
+
+# zz= float(z)
+# print(type(z))
+# print(type(zz))
+"""rasel"""
+a = 0
+
+r = 3
+
+a+=r
+
+a = a + r 
+
+print (a)
+
+
